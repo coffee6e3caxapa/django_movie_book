e1866e2d5608ec4444444c9e40aeb4baedf3e9f3
Doronin Kirill <swaet777@gmail.com>
2022-03-31 22:26:17 +0300

    add func get_object_or_404 в случае если гет выбрасывает исключение

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.show_all_movie),
+    path('', views.show_all_movie),
+    path('movie/<int:id_movie>', views.show_one_movie),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 import datetime
@@ -10,3 +10,8 @@ from . models import Movie
 def show_all_movie(request):
     movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html',{'movies': movies})
+
+def show_one_movie(request, id_movie:str):
+    #movie = Movie.objects.get(id=id_movie)
+    movie = get_object_or_404(Movie,id=id_movie)
+    return render(request, 'movie_app/one_movies.html',{'movie': movie})
